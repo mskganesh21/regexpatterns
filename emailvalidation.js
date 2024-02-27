6f7a9a48b92bd64f341b2f97661015373c37eb45
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-02-27 17:49:12 +0530

    Create emailvalidation.js

---
+++ emailvalidation.js
@@ -0,0 +1,9 @@
+let pattern = /^[a-zA-Z0-9._%+-]+@+(gmail\.com| yahoo\.com)$/
+
+// ^ start of the string
+//$ end of the string
+// [a-zA-Z0-9._%+-] character class which shows atoz, AtoZ, 0to9, . _ % + - characters
+// @ to include @
+//(gmail\.com| yahoo\.com) to include only these
+//$ end of string
+
